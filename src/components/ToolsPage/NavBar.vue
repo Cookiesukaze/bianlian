ed1c45b71cb604eb8308571c56a89679012a9dbb
Cookiesukaze <Cookiesukaze@qq.com>
2024-04-21 09:50:23 +0800

    提交：toolspage重新使用自己的navbar

--- src/components/ToolsPage/NavBar.vue
+++ src/components/ToolsPage/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="navbar grey-border-bottom">
+  <div class="navbar">
     <div class="logo-img" @click="gotoHomePage" style="cursor:pointer;">
       <img src="https://s11.ax1x.com/2023/12/17/pi5amuT.png" alt="Placeholder Image" />
     </div>
@@ -12,40 +12,69 @@
           <a @click="changeLanguage('zh')">{{ $t('language.zh') }}</a>
         </div>
       </div>
-      <div class="dropdown">
-        <button class="dropbtn">{{ $t('about.name') }}</button>
-        <div class="dropdown-content">
-        </div>
-      </div>
-      <div class="dropdown">
+      <div class="dropdown" v-if="!isLoggedIn" @click="toggleLoginCard">
         <button class="dropbtn">{{ $t('login') }}</button>
+      </div>
+      <div class="dropdown" v-else>
+        <button class="dropbtn">{{ username }}</button>
         <div class="dropdown-content">
+          <a @click="logout">{{ $t('logout') }}</a>
         </div>
       </div>
-
     </div>
+    <LoginCard v-if="showLoginCard" @login-success="handleLoginSuccess" style="z-index: 999" class="login-card-container"></LoginCard>
+
   </div>
 </template>
 
 <script>
 
 
+import LoginCard from "@/components/Login/LoginCard.vue";
+
 export default {
+  components: {
+    LoginCard,
+  },
+  data() {
+    return {
+      showLoginCard: false,
+    };
+  },
+  computed: {
+    isLoggedIn() {
+      return this.$store.state.isLoggedIn;
+    },
+    username() {
+      return this.$store.state.username;
+    },
+  },
   methods: {
+    logout() {
+      this.$store.dispatch('logout');
+    },
     changeLanguage(locale) {
       this.$i18n.locale = locale;
     },
     gotoHomePage() {
-      this.$router.push('/');
+      if (this.$route.path === '/mainpage') {
+        location.reload(); // 如果已经在 /mainpage，则刷新页面
+      } else {
+        this.$router.push('/mainpage'); // 否则跳转到 /mainpage
+      }
+    },
+    scrollToDemo(refName) {
+      this.$parent.$refs[refName].$el.scrollIntoView({ behavior: 'smooth' });
+    },
+    toggleLoginCard() {
+      console.log("唤起登录")
+      this.showLoginCard = !this.showLoginCard;
     },
   },
 };
 </script>
 
 <style scoped>
-.grey-border-bottom{
-  border-bottom: 1px solid #e8e8e8;
-}
 .navbar {
   display: flex;
   justify-content: space-between;
@@ -104,4 +133,10 @@ export default {
 }
 
 /* Add styling for other navigation links as needed */
+.login-card-container {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
 </style>
